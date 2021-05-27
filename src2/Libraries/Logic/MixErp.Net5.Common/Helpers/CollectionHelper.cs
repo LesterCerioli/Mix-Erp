803536e9d0e9375986a2703820259d153f8fe743
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-27 16:09:40 -0300

    "fet(#148): implementando classes de Helpers"

---
+++ src2/Libraries/Logic/MixErp.Net5.Common/Helpers/CollectionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixErp.Net5.Common.Helpers
+{
+    public static class CollectionHelper
+    {
+        public static Collection<T> ToCollection<T>(this List<T> items)
+        {
+            if (items == null)
+            {
+                return null;
+            }
+
+
+            Collection<T> Collection = new Collection<T>();
+
+            foreach (T t in items)
+            {
+                Collection.Add(t);
+            }
+
+            return Collection;
+        }
+            
+            
+            
+            
+            
+            
+    }
+
+
+    
+    
+}
--- src2/Libraries/Logic/MixErp.Net5.Common/Helpers/ConfigurationHelper.cs
+++ src2/Libraries/Logic/MixErp.Net5.Common/Helpers/ConfigurationHelper.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace MixErp.Net5.Common.Helpers
 {
-    class ConfigurationHelper
+    public static class ConfigurationHelper
     {
+        public static string GetConfigurationValue(string configFileName, string sectionName)
+        {
+            if (configFileName == null)
+            {
+                return string.Empty;
+            }
+
+            string path = HostingEnvironment.MapPath(ConfigurationManager.AppSettings[configFileName]);
+
+            ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap { ExeConfigFilename = path };
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap,
+                ConfigurationUserLevel.None);
+            AppSettingsSection section = config.GetSection("appSettings") as AppSettingsSection;
+            
+            if (section != null)
+            {
+                if (section.Settings[sectionName] != null)
+                {
+                    return section.Settings[sectionName].Value;
+                }
+            }
+
+            return string.Empty;
+
+
+
+
+        }
     }
+    public static string GetDbServerParameter(string key)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
