04e2adf99e26c907b37ad65e717e0348201eab5f
lester <lesterlucasit@hotmail.com>
2021-05-31 21:11:27 -0400

    fet(#158): implementing Governor and Transaction microservice

--- src2/src/Modules/Core/MixErp.Modules.Core/Models/Role.cs
+++ src2/src/Modules/Core/MixErp.Modules.Core/Models/Role.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using MixErp.Infrastructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace MixErp.Modules.Core.Models
 {
-    class Role
+    public class Role : IdentityRole<long>, IEntityWithTypedId<long>
     {
+         public IList<UserRole> Users { get; set; } = new List<UserRole>();
+
+        public long id => throw new NotImplementedException();
     }
 }
--- src2/src/Modules/Core/MixErp.Modules.Core/Models/WeekDay.cs
+++ src2/src/Modules/Core/MixErp.Modules.Core/Models/WeekDay.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MixErp.Modules.Core.Models
 {
-    class WeekDay
+    public class WeekDay
     {
     }
 }
