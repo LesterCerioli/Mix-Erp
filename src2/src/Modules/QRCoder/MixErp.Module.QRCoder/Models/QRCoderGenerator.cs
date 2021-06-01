ca058f467e90568dbc43cb0c71593a45a0070d68
lester <lesterlucasit@hotmail.com>
2021-06-01 16:54:25 -0400

    fet(#157): implementando o microserviço0 de QRCode

---
+++ src2/src/Modules/QRCoder/MixErp.Module.QRCoder/Models/QRCoderGenerator.cs
@@ -0,0 +1,30 @@
+namespace MixErp.Module.QRCoder.Models
+{
+    public static class QRCoderGenerator
+    {
+        public static Bitmap GenerateImage(string url)
+        {
+            var qrGenerator = new QRCoderGenerator();
+            var qrCoderData = qrGenerator.CreateCode(url, QRCoderGeneratorECCLevel.Q);
+            var QRCoder = new QRCoder(qrCoderData);
+            var qrCoderImage = qrCoder.Getgraphic(10);
+            return qrCoderImage;
+
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var iamage = GenerateImage(url);
+            return ImageToByte(image);
+
+        }
+
+        private static byte[] ImageToByte(Image imag)
+        {
+            using bar stream = new MemoryStream();
+            imag.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+        
+    }
+}
